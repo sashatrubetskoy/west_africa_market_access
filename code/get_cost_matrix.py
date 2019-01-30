99ade6e1cbc7ae398742cabf750b8c1695d799df
Sasha Trubetskoy <sasha@Sashas-iMac.local>
2019-01-30 14:30:38 -0600

    nearest node candidates now forced within same country

--- code/get_cost_matrix.py
+++ code/get_cost_matrix.py
@@ -2,16 +2,18 @@
 # January 2019
 # trub@uchicago.edu
 
-import json
-import time
-import string
-import random
-import logging
 import argparse
 import itertools
+import json
+import logging
+import pickle
+import random
+import string
+import time
 import numpy as np
 import pandas as pd
 import networkx as nx
+from collections import OrderedDict
 from scipy import spatial, special
 from tqdm import tqdm, tqdm_pandas
 
@@ -46,14 +48,14 @@ logger.info('Cost matrix will export to {}.'.format(args.outfile))
 #---------------------------------------------------
 def read_geojsons():
 
-    def features_to_graph(features, z):
+    def features_to_graph(features, iso):
         # Directed graph is necessary because borders have
         # asymmetric costs
         G = nx.DiGraph() 
         for line in features:
-            start = tuple(line['geometry']['coordinates'][0][0]+[z])
-            end = tuple(line['geometry']['coordinates'][0][-1]+[z])
-            
+            start = tuple(line['geometry']['coordinates'][0][0]+[iso])
+            end = tuple(line['geometry']['coordinates'][0][-1]+[iso])
+
             # Add edge in both directions
             k = G.add_edge(start, end)
             k = G.add_edge(end, start)
@@ -77,9 +79,9 @@ def read_geojsons():
 
     ## Create & compose the graphs one at a time
     road = nx.DiGraph()
-    for i, iso in enumerate(countries):
+    for iso in countries:
         road_features_i = [feature for feature in road_json['features'] if feature['properties']['iso3']==iso]
-        G_i = features_to_graph(road_features_i, z=(100+i))
+        G_i = features_to_graph(road_features_i, iso)
         road = nx.compose(road, G_i)
     
     ## Sea is separate. Rail is not implemented.
@@ -88,7 +90,7 @@ def read_geojsons():
 
     with open(SEA_FILE, 'r') as f:
         sea_json = json.load(f)
-    sea = features_to_graph(sea_json['features'], z=500)
+    sea = features_to_graph(sea_json['features'], 'sea')
 
     G = nx.compose(road, rail)
     G = nx.compose(G, sea)
@@ -99,28 +101,59 @@ def read_geojsons():
 
 # 2. Find nearest nodes to cities
 #---------------------------------------------------
+def get_highest_quality_node_within_radius(point, iso3, nodelist, g, r=0.05):
+    nodes_just_xy = np.array([(node[0], node[1]) for node in nodelist], dtype=np.float64) # Ignore country
+    kdtree_query_result_node_ids = spatial.KDTree(nodes_just_xy).query(point, k=500, distance_upper_bound=r)[1] # Use scipy magic to quickly get nearest nodes. Get 0.1 degrees = ~10 km
+    all_within_r_ids = list(OrderedDict.fromkeys(kdtree_query_result_node_ids)) # Remove duplicates preserving order
+    all_within_r_ids = [a for a in all_within_r_ids if a != len(nodelist)] # Since the result is padded with placeholder value if fewer than k are found
+    all_within_r = [tuple(nodelist[i]) for i in all_within_r_ids] # Get actual nodes
+    
+    # Make sure nodes are within the city's country
+    all_within_r = [node for node in all_within_r if node[2]==iso3]
+
+    # If there are no nodes within radius, just take the closest node
+    if not all_within_r:
+        idx = spatial.KDTree(nodes_just_xy).query(point)[1]
+        return tuple(nodelist[idx])
+
+    # Find highest quality level within radius
+    def max_quality(node):
+        best_q_dest = sorted(g[node], key=lambda x: g[node][x]['quality'])[-1]
+        return g[node][best_q_dest]['quality']
+
+    best_quality = 0
+    for candidate in all_within_r:
+        candidate_max_quality = max_quality(candidate)
+        if candidate_max_quality > best_quality:
+            best_quality = candidate_max_quality
+
+    # Find closest node of that quality
+    return next(node for node in all_within_r if max_quality(node)==best_quality)
+
+
 def match_cities_with_nodes(road, rail, sea, G):
     cities = pd.read_csv(CITIES_CSV, converters={'nearest_road': eval, 'nearest_rail': eval, 'nearest_sea': eval, 'nearest_any': eval})
-    road_nodes = np.array(list(road.nodes))
-    rail_nodes = np.array(list(rail.nodes))
-    sea_nodes = np.array(list(sea.nodes))
-    any_nodes = np.array(list(G.nodes))
+    road_nodes = list(road.nodes)
+    rail_nodes = list(rail.nodes)
+    sea_nodes = list(sea.nodes)
+    any_nodes = list(G.nodes)
 
     # if ('nearest_road' not in cities.columns) or (args.force_rematch):
     logger.info('2. Matching cities with nodes...')
     def get_nearest_node(row):
         point = list(row[['X', 'Y']]) # X, Y coordinates of city in projection
-        row['nearest_road'] = tuple(road_nodes[spatial.KDTree(road_nodes[:,:2]).query(point)[1]]) # scipy magic
-        # row['nearest_rail'] = tuple(rail_nodes[spatial.KDTree(rail_nodes).query(point)[1]])
-        row['nearest_sea'] = tuple(sea_nodes[spatial.KDTree(sea_nodes[:,:2]).query(point)[1]])
-        row['nearest_any'] = tuple(any_nodes[spatial.KDTree(any_nodes[:,:2]).query(point)[1]])
+        iso3 = row['iso3']
+        row['nearest_road'] = get_highest_quality_node_within_radius(point, iso3, road_nodes, road)
+        # row['nearest_rail'] = get_highest_quality_node_within_radius(point, rail_nodes, rail)
+        row['nearest_sea'] = tuple(sea_nodes[spatial.KDTree(np.array([(n[0], n[1]) for n in sea_nodes], dtype=np.float64)).query(point)[1]])
+        # row['nearest_any'] = sorted([row['nearest_road'], row['nearest_sea']],
+        #     key=lambda x: np.linalg.norm(np.array(point) - np.array(x[:2]))\
+        #     )[0]
+        row['nearest_any'] = row['nearest_road']
         return row
-
     cities = cities.progress_apply(get_nearest_node, axis=1)
     cities.to_csv(CITIES_CSV, index=False)
     logger.info('\nCities matched.')
-    # else:
-    #     logger.info('2. Cities already matched.')
         
     return cities, road_nodes, rail_nodes, sea_nodes, any_nodes
 #---------------------------------------------------
@@ -133,6 +166,7 @@ def add_costs_to_graph(G):
     for u, v in G.edges:
         if 'cost' not in G[u][v]: # careful: costs are per km, length is in meters
             G[u][v]['cost'] = TCOST[str(G[u][v]['quality'])] * G[u][v]['length']/1000
+            G[v][u]['cost'] = TCOST[str(G[v][u]['quality'])] * G[v][u]['length']/1000
     logger.info('Costs added.')
     return G
 #---------------------------------------------------
@@ -140,19 +174,25 @@ def add_costs_to_graph(G):
 
 # 4. Find nearest nodes to ports
 #---------------------------------------------------
-def find_nearest_nodes_to_ports(road_nodes, rail_nodes, sea_nodes, any_nodes):
-    logger.info('4. Matching ports with nodes...')
+def find_nearest_nodes_to_ports(road, rail, sea, G):
     with open(PORTS_FILE, 'r') as p:
         ports_geojson = json.load(p)
     ports_nodes = [tuple(f['geometry']['coordinates']) for f in ports_geojson['features']]
     ports = pd.DataFrame(ports_nodes, columns=['X', 'Y'])
-
+    road_nodes = list(road.nodes)
+    rail_nodes = list(rail.nodes)
+    sea_nodes = list(sea.nodes)
+    any_nodes = list(G.nodes)
+    
+    logger.info('4. Matching ports with nodes...')
     def get_nearest_node(row):
         point = list(row[['X', 'Y']]) # X, Y coordinates of city in projection
-        row['nearest_road'] = tuple(road_nodes[spatial.KDTree(road_nodes[:,:2]).query(point)[1]]) # scipy magic
-        # row['nearest_rail'] = tuple(rail_nodes[spatial.KDTree(rail_nodes).query(point)[1]])
-        row['nearest_sea'] = tuple(sea_nodes[spatial.KDTree(sea_nodes[:,:2]).query(point)[1]])
-        row['nearest_any'] = tuple(any_nodes[spatial.KDTree(any_nodes[:,:2]).query(point)[1]])
+        row['nearest_road'] = get_highest_quality_node_within_radius(point, 'sea', road_nodes, road)
+        # row['nearest_rail'] = get_highest_quality_node_within_radius(point, rail_nodes, rail)
+        row['nearest_sea'] = tuple(sea_nodes[spatial.KDTree(np.array([(n[0], n[1]) for n in sea_nodes], dtype=np.float64)).query(point)[1]])
+        row['nearest_any'] = sorted([row['nearest_road'], row['nearest_sea']],
+            key=lambda x: np.linalg.norm(np.array(point) - np.array(x[:2]))\
+            )[0]
         return row
 
     ports = ports.progress_apply(get_nearest_node, axis=1)
@@ -167,7 +207,7 @@ def find_nearest_nodes_to_ports(road_nodes, rail_nodes, sea_nodes, any_nodes):
 #     logger.info('4. Creating road-rail transfers...')
 #     for i in range(len(cities)):
 #         x, y, u, v = list(cities.iloc[i][['X', 'Y', 'nearest_road', 'nearest_rail']])
-#         if np.sqrt((x-v[0])**2 + (y-v[1])**2) < 10000:
+#         if np.sqrt((x-v[0])**2 + (y-v[1])**2) < 0.1:
 #             G.add_edge(u, v,
 #                 len_km=0,
 #                 class_0='transfer',
@@ -185,16 +225,16 @@ def create_sea_transfers(ports, G):
     logger.info('5. Creating sea transfers...')
     for i in range(len(ports)):
         x, y, u, v = list(ports.iloc[i][['X', 'Y', 'nearest_road', 'nearest_sea']])
-        port_is_near_road = np.sqrt((x-u[0])**2 + (y-u[1])**2) < 10000
-        port_is_near_sea_link = np.sqrt((x-v[0])**2 + (y-v[1])**2) < 10000
+        port_is_near_road = np.sqrt((x-u[0])**2 + (y-u[1])**2) < 0.05
+        port_is_near_sea_link = np.sqrt((x-v[0])**2 + (y-v[1])**2) < 0.05
         if port_is_near_road and port_is_near_sea_link:
             G.add_edge(u, v,
                 length=0,
-                quality='transfer',
+                quality='port_fee',
                 cost=PARAMS['port_fee'])
             G.add_edge(v, u,
                 length=0,
-                quality='transfer',
+                quality='port_fee',
                 cost=PARAMS['port_fee'])
     logger.info('Sea transfers created.')
     return G
@@ -213,7 +253,7 @@ def create_border_crossings(road_nodes, G):
 
         # Find which two nodes match the border crossing X,Y
         coords = bc['geometry']['coordinates']
-        matches = [i for i, row in enumerate(road_nodes[:,:2]) if all(np.round(row, 8) == np.round(coords, 8))]
+        matches = [i for i, row in enumerate(np.array([(n[0], n[1]) for n in road_nodes], dtype=np.float64)) if all(np.round(row, 6) == np.round(coords, 6))]
         if not matches:
             logger.info('\tThere is no road node at {}!'.format(coords))
             continue
@@ -229,6 +269,10 @@ def create_border_crossings(road_nodes, G):
         # use that cost. Otherwise default to BCOST csv.
         cost_a = bc['properties']['border_cost'] if bc['properties']['border_cost'] != -1 else BCOST.loc[country_a][country_b]
         cost_b = bc['properties']['border_cost'] if bc['properties']['border_cost'] != -1 else BCOST.loc[country_b][country_a]
+        # cost_a = 0
+        # cost_b = 0
+        if min([cost_a, cost_b]) < 0:
+            logger.warning('Border cost is less than zero!')
 
         G.add_edge(a, b,
             length=0,
@@ -249,7 +293,6 @@ def get_cost_matrix(cities, G):
     all_cities = cities['ORIG_FID'].tolist() # Field just needs to be a unique ID
     country_of = cities.set_index('nearest_any').to_dict()['iso3']
     nearest_node = cities.set_index('ORIG_FID').to_dict()['nearest_any']
-    
     matrix_dict = dict()
 
     counter = 0
@@ -265,7 +308,6 @@ def get_cost_matrix(cities, G):
             end='\r')
 
         city_a_node = nearest_node[city_a]
-        city_a_node = (city_a_node[0], city_a_node[1], int(city_a_node[2]))
         costs, paths = nx.single_source_dijkstra(G, city_a_node, weight='cost')
         costs_cities = []
 
@@ -280,6 +322,7 @@ def get_cost_matrix(cities, G):
             transport_cost = costs[city_b_node] / PARAMS['shipment_value']
             # Tariffs at destination [ad valorem]
             tariff = TARIFF.loc[country_of[city_b_node]]['tariff']
+            # tariff = 0
 
             if country_of[city_a_node] == country_of[city_b_node]:
                 final_cost = transport_cost
@@ -308,10 +351,13 @@ def setup():
     cities, road_nodes, rail_nodes, sea_nodes, any_nodes = match_cities_with_nodes(road, rail, sea, G)
     G = add_costs_to_graph(G)
     # G = create_road_rail_transfers(cities, G)
-    ports = find_nearest_nodes_to_ports(road_nodes, rail_nodes, sea_nodes, any_nodes)
+    ports = find_nearest_nodes_to_ports(road, rail, sea, G)
     G = create_sea_transfers(ports, G)
     G = create_border_crossings(road_nodes, G)
+    pickle.dump([(e, G[e[0]][e[1]]['cost'], G[e[1]][e[0]]['cost']) for e in G.edges], open('test', 'wb'))
     # G, externals = set_up_external_markets(ports, G)
+    # with open('edges.txt', 'w') as f:
+    #     f.writelines([str(e)+'\n' for e in list(G.edges)])
     return road, rail, sea, G, cities
 
 
